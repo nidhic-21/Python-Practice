792ac7e27f270bc87baec10c870ba45fc12859c6
nidhic-21 <nidhi.chaudhary@cumminscollege.edu.in>
2026-06-15 21:42:37 +0530

    Implement logic to find the largest of three numbers

---
+++ 02_largest_number.py
@@ -0,0 +1,10 @@
+a = eval(input("Enter a number a :"))
+b = eval(input("Enter a number b :"))
+c = eval(input("Enter a number c :"))
+if a>b and a>c:
+  print(f" {a} is the largest number.")
+elif b>a and b>c:
+  print(f" {b} is the largest number.")
+else:
+  (f" {c} is the largest number.") 
+  
